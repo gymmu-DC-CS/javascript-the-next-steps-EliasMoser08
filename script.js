64829697f66d2dc56a276a232d231a9ad6121d6d
EliasMoser08 <142326934+EliasMoser08@users.noreply.github.com>
2023-12-19 15:23:11 +0100

    solved exercise 4

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 export function exercise01(args) {
   const input = args
   const result = []
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
- if (currentElement === "e") {
-  result.push("")
- } else if (currentElement === "E") {
-  result.push("")
- } else {
-  result.push(currentElement)
- }
-}
+    if (currentElement === "e") {
+      result.push("")
+    } else if (currentElement === "E") {
+      result.push("")
+    } else {
+      result.push(currentElement)
+    }
+  }
   return result.join("")
 }
 
 export function exercise02(args) {
   const input = args
   const result = []
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     const capitalizedElement = currentElement.toUpperCase()
@@ -27,20 +27,35 @@ export function exercise02(args) {
   return result.join("")
 }
 
-
 export function exercise03(args) {
   const input = args
   const result = []
   let count = 0
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-if (currentElement === 'e'){
-  count = count + 1
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
 }
-else if (currentElement === 'E'){
-  count = count + 1
-}
+
+export function exercise04(args) {
+  const input = args
+  let count = 1
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    const previousElement = input[i - 1]
+
+    if (currentElement === " " && previousElement != "-") {
+      count = count + 1
+    } else if (currentElement === "0") {
+      count = count - 1
+    }
   }
   return count
 }
